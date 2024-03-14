8b0aaec3dc4ae54713bb235a7a0151752368d913
杨枫 <2867108693@qq.com>
2024-03-14 14:30:52 +0800

    Exercise

---
+++ Exercise2.java
@@ -0,0 +1,176 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Exercise2 {
+    static  public  void main(String[] args)
+    {
+//        int number1, number2, option, result = 0;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter two numbers");
+//        number1  = sc.nextInt();
+//        number2 = sc.nextInt();
+//        System.out.println("Enter a number between 1 to 5");
+//        option = sc.nextInt();
+//        switch (option) {
+//            case 1:
+//                System.out.println("You enter 1");
+//                result = number1 + number2;
+//                break;
+//            case 2:
+//                System.out.println("You entered 2");
+//                result = number1 > number2 ? number1 - number2 : number2 - number1;
+//                break;
+//            case 3:
+//                System.out.println("You entered 3");
+//                result = number1 * number2;
+//                break;
+//            case 4:
+//                System.out.println("You entered 4");
+//                result = number1 / number2;
+//                break;
+//            case 5:
+//                System.out.println("close");
+//                break;
+//            default:
+//                System.out.println("incorrect input");
+//                break;
+//        }
+//        System.out.println("The result is:" + result);
+
+
+//        int counter = 0;
+//        double acount = 0;
+//        Scanner sc = new Scanner(System.in);
+//        acount = sc.nextInt();
+//        while(acount < 1000000)
+//        {
+//            counter++;
+//            acount += acount*(0.05);
+//        }
+//
+//        System.out.println(counter);
+
+
+
+//        for(int i = 1; i <= 20; i++)
+//        {
+//            System.out.println(i);
+//        }
+
+
+//        int[] array = {1, 21, 3, 4, 5, 60, 7, 8, 100, 120};
+//        for(int i = 0; i < 10; i++)
+//        {
+//            if(array[i] % 3 == 0 && array[i] < 100)
+//            {
+//                System.out.println(array[i]);
+//            }
+//        }
+
+
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Please enter the letter");
+//        String letter = sc.next();
+//        System.out.println("Please enter the number");
+//        int number = sc.nextInt();
+//        if(letter.equals("a") || letter.equals("c") || letter.equals("e") || letter.equals("g"))
+//        {
+//            if(number % 2 != 0)
+//            {
+//                System.out.println("Black");
+//            }
+//            else
+//            {
+//                System.out.println("White");
+//            }
+//        }
+//        else
+//        {
+//            if(number % 2 != 0)
+//            {
+//                System.out.println("White");
+//            }
+//            else
+//            {
+//                System.out.println("Black");
+//            }
+//        }
+
+
+//        Scanner sc = new Scanner(System.in);
+//        int number = 1, sum = 0, count = 0;
+//        while(number != 0)
+//        {
+//            number = sc.nextInt();
+//            sum += number;
+//            if(number != 0)
+//            {
+//                count++;
+//            }
+//        }
+//        System.out.println(sum / count);
+
+
+//        int[] array = new int[20];
+//        array[1] = 1;
+//        for(int i = 2; i < 20; i++)
+//        {
+//            array[i] = array[i-2] + array[i-1];
+//        }
+//
+//        for(int i = 0; i < array.length; i++)
+//        {
+//            System.out.println(array[i]);
+//        }
+
+
+
+//        double total = 0;
+//        double salary = 0.01;
+//
+//        for (int day = 1; day <= 30; day++) {
+//            System.out.println("Day " + day + ": £" + salary);
+//            total += salary;
+//            salary *= 2;
+//        }
+//
+//        System.out.println("Total at the end of the month: £" + total);
+
+
+//        int[] marks = new int[20];
+//        Random random = new Random();
+//
+//
+//        for (int i = 0; i < marks.length; i++) {
+//            marks[i] = random.nextInt(101);
+//        }
+//
+//        int sum = 0, max = 0, min = 100, pass = 0;
+//        for (int mark : marks) {
+//            sum += mark;
+//            if (mark > max) {
+//                max = mark;
+//            }
+//            if (mark < min) {
+//                min = mark;
+//            }
+//            if (mark >= 40) {
+//                pass++;
+//            }
+//        }
+//
+//        double average = sum / marks.length;
+//
+//        System.out.println("average: " + average);
+//        System.out.println("highest mark: " + max);
+//        System.out.println("lowest mark: " + min);
+//        System.out.println("pass numbers: " + pass);
+
+
+
+
+    }
+
+
+}
